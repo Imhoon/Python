b8eef7769295df476fdd2176579d51d01edb8573
JaeHoon <87082793+Imhoon@users.noreply.github.com>
2022-04-19 14:36:04 +0900

    Add files via upload

---
+++ read.py
@@ -0,0 +1,22 @@
+f = open("새파일.text",'')
+#'r'읽기모드로 파일을 열어서
+#while True:
+ #   line = f.readline()
+  #  if not line: break
+   # print(line)
+
+for i in range(20):
+    i+=1
+    f. write(str(i)+"번째 줄입니다.")
+f.close()
+
+#문제
+#새로운 파일 내용 추가하기 
+#'a' : add모드로 파일을 열어서
+#for문으로 11 ~ 20번째 줄입니다 작성해서
+
+#visual code 에 1~20번째 줄입니다 출력하기
+
+#2. 문제
+#10부터 30까지의 홀수만 파일에 한줄씩 작성하기
+#작성된 파일에서 29까지만 더해서 결과 출력하기
